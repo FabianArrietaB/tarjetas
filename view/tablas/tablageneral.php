83e3977b908959036675555042e45172033d7404
FabianArrietaB <fabianarrieta03@gmail.com>
2023-08-03 20:40:28 -0500

    2023

--- view/tablas/tablageneral.php
+++ view/tablas/tablageneral.php
@@ -26,7 +26,7 @@
     if($sede != ""){
         $sqlgeneral .=" AND r.id_sede = '$sede'";
     }
-    $sqlgeneral .="GROUP BY r.reg_tiptar ORDER BY r.id_registro DESC ";
+    $sqlgeneral .="GROUP BY r.reg_tiptar";
     $rwgeneral = mysqli_query($conexion, $sqlgeneral);
     $sqlunico = "SELECT
         r.id_registro     AS idregistro,
@@ -44,117 +44,12 @@
     if($sede != ""){
         $sqlunico .=" AND r.id_sede = '$sede'";
     }
-    $sqlunico .="GROUP BY r.id_operador, r.reg_tiptar ORDER BY r.id_registro DESC ";
+    $sqlunico .="GROUP BY r.reg_tiptar, r.id_operador";
     $rwunico = mysqli_query($conexion, $sqlunico);
 ?>
 <!-- inicio Tabla -->
-<div class="row">
-    <!-- <div class="col-6">
-        <div class="table-responsive">
-            <table class="table table-light text-center">
-                <thead>
-                    <tr>
-                        <th scope="col" >Tarjeta | Caja</th>
-                        <th scope="col" >Diferencia</th>
-                        <th scope="col" >Rte Fte</th>
-                        <th scope="col" >Rte IVA</th>
-                        <th scope="col" >Rte ICA</th>
-                        <th scope="col" >Comision</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                        while ($valor = mysqli_fetch_array($rwgeneral)){
-                    ?>
-                    <tr>
-                        <td><?php echo $valor['tiptar'];?></td>
-                        <td><?php echo '$ ' . number_format(round($valor['diferencia']));?></td>
-                        <td><?php echo '$ ' . number_format(round($valor['retefuente']));?></td>
-                        <td><?php echo '$ ' . number_format(round($valor['reteiva']));?></td>
-                        <td><?php echo '$ ' . number_format(round($valor['reteica']));?></td>
-                        <td><?php echo '$ ' . number_format(round($valor['comision']));?></td>
-                    </tr>
-                    <?php } ?>
-                </tbody>
-                <TFoot>
-                    <tr>
-                        <td class="bg-grays-active color-palette"><b>Total </b></td>
-                        <td>
-                        <?php
-                        if($sede != "" && $hoy != ""){
-                            $sql=$conexion->query("SELECT round(SUM(reg_diferencia)) as 'precio' from registros where id_sede = '$sede' AND reg_fecope = '$hoy' ");
-                            $data = mysqli_fetch_array($sql);
-                            $precio = $data['precio'];
-                            echo '$ '. number_format($precio);
-                        } else {
-                            echo '$ '. 0;
-                        }
-                        ?>
-                    </td>
-                    <td>
-                        <?php
-                        if($sede != "" && $hoy != ""){
-                            $sql=$conexion->query("SELECT round(SUM(reg_rtefte)) as 'rtefte' from registros where id_sede = '$sede' AND reg_fecope = '$hoy'");
-                            $data = mysqli_fetch_array($sql);
-                            $precio = $data['rtefte'];
-                            echo '$ '. number_format($precio);
-                        } else {
-                            
-                            echo '$ '. 0;
-                        }
-                        ?>
-                    </td>
-                    <td>
-                        <?php
-                            if($sede != "" && $hoy != ""){
-                            $sql=$conexion->query("SELECT round(SUM(reg_rteiva)) as 'rteiva' from registros where id_sede = '$sede' AND reg_fecope = '$hoy'");
-                            $data = mysqli_fetch_array($sql);
-                            $precio = $data['rteiva'];
-                            echo '$ '. number_format($precio);
-                        } else {
-                            
-                            echo '$ '. 0;
-                        }
-                        ?>
-                    </td>
-                    <td>
-                        <?php
-                        if($sede != "" && $hoy != ""){
-                            $sql=$conexion->query("SELECT round(SUM(reg_rteica)) as 'rteica' from registros where id_sede = '$sede' AND reg_fecope = '$hoy'");
-                            $data = mysqli_fetch_array($sql);
-                            $precio = $data['rteica'];
-                            echo '$ '. number_format($precio);
-                        } else {
-                            
-                            echo '$ '. 0;
-                        }
-                        ?>
-                    </td>
-                    <td>
-                        <?php
-                        if($sede != "" && $hoy != ""){
-                            $sql=$conexion->query("SELECT round(SUM(reg_comision)) as 'comision' from registros where id_sede = '$sede' AND reg_fecope = '$hoy'");
-                            $data = mysqli_fetch_array($sql);
-                            $precio = $data['comision'];
-                            echo '$ '. number_format($precio);
-                        } else {
-                            echo '$ '. 0;
-                        }
-                        ?>
-                    </td>
-                    </tr>
-                    <tr>
-                        <td class="bg-grays-active color-palette">Banco</td>
-                    </tr>
-                    <tr>
-                        <td class="bg-grays-active color-palette">Diferencia</td>
-                    </tr>
-                </TFoot>
-            </table>
-        </div>
-    </div> -->
-    <div class="col-12">
-    <div class="table-responsive">
+
+<div class="table-responsive">
     <table class="table table-light text-center">
         <thead>
             <tr>
@@ -180,23 +75,100 @@
             </tr>
             <?php } ?>
         </tbody>
-        <TFoot>
-            <?php while ($valor = mysqli_fetch_array($rwgeneral)){ ?>
-            <tr>
-                <td>Total</td>
-                <tr>
-                
-                    <td><?php echo $valor['diferencia'];?></td>
-                
-                </tr>
-            </tr>
-            <?php } ?>
-        </TFoot>
     </table>
-</div>
+    <div class="container-fluid">
+        <form id="frmadddiferencia" method="post" onsubmit="return adddiferencia()">
+            <?php
+                while ($valor = mysqli_fetch_array($rwgeneral)){
+            ?>
+            <div class="row text-center">
+                <div class="col-2">
+                    <div class="mb-2">
+                        <label for="exampleFormControlInput1" class="form-label"><?php echo $valor['tiptar'] ?></label>
+                        <input hidden type="text">
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="mb-2">
+                        <label for="exampleFormControlInput1" class="form-label">TOTAL DIFERENCIAS</label>
+                        <input type="text" class="form-control text-center" name="bandif" id="bandif" value="<?php echo '$ ' . number_format(round($valor['diferencia']))?>" placeholder="000000" required>
+                    </div>
+                    <div class="mb-2">
+                        <label for="exampleFormControlInput1" class="form-label">VALOR BANCO</label>
+                        <input type="text" class="form-control text-center" name="difban" id="difban" placeholder="000000" required>
+                    </div>
+                    <div class="mb-2">
+                        <label for="exampleFormControlInput1" class="form-label">DIFERENCIA</label>
+                        <input type="text" class="form-control text-center" name="difbanbd" id="difbanbd" placeholder="000000" readonly>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="mb-2">
+                        <label for="exampleFormControlInput1" class="form-label">TOTAL RETEFUENTE</label>
+                        <input type="text" class="form-control text-center" name="banretfte" id="banretfte" value="<?php echo '$ ' . number_format(round($valor['retefuente']))?>" placeholder="000000" required>
+                    </div>
+                    <div class="mb-2">
+                        <label for="exampleFormControlInput1" class="form-label">VALOR BANCO</label>
+                        <input type="text" class="form-control text-center" name="difban" id="difban" placeholder="000000" required>
+                    </div>
+                    <div class="mb-2">
+                        <label for="exampleFormControlInput1" class="form-label">DIFERENCIA</label>
+                        <input type="text" class="form-control text-center" name="banvsbd" id="banvsbd" placeholder="000000" readonly>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="mb-2">
+                        <label for="exampleFormControlInput1" class="form-label">TOTAL RETEIVA</label>
+                        <input type="text" class="form-control text-center" name="banretiva" id="banretiva" value="<?php echo '$ ' . number_format(round($valor['reteiva']))?>" placeholder="000000" required>
+                    </div>
+                    <div class="mb-2">
+                        <label for="exampleFormControlInput1" class="form-label">VALOR BANCO</label>
+                        <input type="text" class="form-control text-center" name="difban" id="difban" placeholder="000000" required>
+                    </div>
+                    <div class="mb-2">
+                        <label for="exampleFormControlInput1" class="form-label">DIFERENCIA</label>
+                        <input type="text" class="form-control text-center" name="banvsbd" id="banvsbd" placeholder="000000" readonly>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="mb-2">
+                        <label for="exampleFormControlInput1" class="form-label">TOTAL RETEICA</label>
+                        <input type="text" class="form-control text-center" name="banretica" id="banretica" value="<?php echo '$ ' . number_format(round($valor['reteica']))?>" placeholder="000000" required>
+                    </div>
+                    <div class="mb-2">
+                        <label for="exampleFormControlInput1" class="form-label">VALOR BANCO</label>
+                        <input type="text" class="form-control text-center" name="difban" id="difban" placeholder="000000" required>
+                    </div>
+                    <div class="mb-2">
+                        <label for="exampleFormControlInput1" class="form-label">DIFERENCIA</label>
+                        <input type="text" class="form-control text-center" name="banvsbd" id="banvsbd" placeholder="000000" readonly>
+                    </div>
+                </div>
+                <div class="col-2">
+                    <div class="mb-2">
+                        <label for="exampleFormControlInput1" class="form-label">TOTAL COMISION</label>
+                        <input type="text" class="form-control text-center" name="bancomi" id="bancomi" value="<?php echo '$ ' . number_format(round($valor['comision']))?>" placeholder="000000" required>
+                    </div>
+                    <div class="mb-2">
+                        <label for="exampleFormControlInput1" class="form-label">VALOR BANCO</label>
+                        <input type="text" class="form-control text-center" name="difban" id="difban" placeholder="000000" required>
+                    </div>
+                    <div class="mb-2">
+                        <label for="exampleFormControlInput1" class="form-label">DIFERENCIA</label>
+                        <input type="text" class="form-control text-center" name="banvsbd" id="banvsbd" placeholder="000000" readonly>
+                    </div>
+                </div>
+            </div>
+            <?php } ?>
+                <div class="row">
+                    <div class="col-12">
+                        <div >
+                            <button type="submit" class="btn btn-success" >Agregar</button>
+                        </div>
+                    </div>
+                </div>
+           
+        </form>
     </div>
 </div>
-
-
-
 <!-- fin de la tabla -->
