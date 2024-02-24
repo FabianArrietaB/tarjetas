58297ee07e02f7c3bdc23100f00f6c9261711ac3
FabianArrietaB <fabianarrieta03@gmail.com>
2024-02-24 15:01:01 -0500

    2023

--- view/registros.php
+++ view/registros.php
@@ -49,7 +49,7 @@
                     <form id="frmaddregistro" method="post" onsubmit="return validar()">
                         <div class="row text-center">
                             <input hidden type="text" class="form-control" name="tiptar" id="tiptar" placeholder="">
-                            <div class="col-xs-12 col-sm-4 col-md-2">
+                            <div class="col-xs-12 col-sm-6 col-md-2">
                                 <div class="mb-3">
                                     <label for="exampleFormControlInput1" class="form-label">Datafono</label>
                                     <select id="iddatafo" name="iddatafo" class="form-select" aria-label="Default select example" required>
@@ -75,13 +75,13 @@
                                     </select>
                                 </div>
                             </div>
-                            <div class="col-xs-12 col-sm-1 col-md-1">
+                            <div class="col-xs-12 col-sm-2 col-md-1">
                                 <div class="mb-3">
                                     <label for="exampleFormControlInput1" class="form-label">%</label>
                                     <input type="text" class="form-control"  name="portar" id="portar" placeholder="000" required>
                                 </div>
                             </div>
-                            <div class="col-xs-12 col-sm-2 col-md-2">
+                            <div class="col-xs-12 col-sm-6 col-md-2">
                                 <div class="mb-3">
                                     <label for="exampleFormControlInput1" class="form-label">Ticket</label>
                                     <div class="input-group">
@@ -96,19 +96,19 @@
                                     </div>
                                 </div>
                             </div>
-                            <div class="col-xs-12 col-sm-2 col-md-2">
+                            <div class="col-xs-12 col-sm-6 col-md-2">
                                 <div class="mb-3">
                                     <label for="exampleFormControlInput1" class="form-label">Valor</label>
                                     <input type="text" class="form-control" name="valor" id="valor" placeholder="000000" required>
                                 </div>
                             </div>
-                            <div class="col-xs-12 col-sm-2 col-md-2">
+                            <div class="col-xs-12 col-sm-6 col-md-2">
                                 <div class="mb-3">
                                     <label for="exampleFormControlInput1" class="form-label">Iva</label>
                                     <input type="text" class="form-control" name="iva" id="iva" placeholder="000000" required>
                                 </div>
                             </div>
-                            <div class="col-xs-12 col-sm-1 col-md-1">
+                            <div class="col-xs-12 col-sm-6 col-md-1">
                                 <div class="mb-3">
                                     <label for="exampleFormControlInput1" class="form-label">Neto</label>
                                     <input type="text" class="form-control" name="neto" id="neto" placeholder="000000" readonly>
@@ -116,37 +116,37 @@
                             </div>
                         </div>
                         <div class="row">
-                        <div class="col-xs-12 col-sm-2 col-md-2">
+                            <div class="col-xs-12 col-sm-6 col-md-2">
                                 <div class="mb-3">
                                     <label for="exampleFormControlInput1" class="form-label">Rete Fuente</label>
                                     <input type="text" class="form-control" name="retfue" id="retfue" placeholder="000000" readonly>
                                 </div>
                             </div>
-                            <div class="col-xs-12 col-sm-2 col-md-2">
+                            <div class="col-xs-12 col-sm-6 col-md-2">
                                 <div class="mb-3">
                                     <label for="exampleFormControlInput1" class="form-label">Comision</label>
                                     <input type="text" class="form-control" name="comisi" id="comisi" placeholder="000000" readonly>
                                 </div>
                             </div>
-                            <div class="col-xs-12 col-sm-2 col-md-2">
+                            <div class="col-xs-12 col-sm-6 col-md-2">
                                 <div class="mb-3">
                                     <label for="exampleFormControlInput1" class="form-label">Rete IVA</label>
                                     <input type="text" class="form-control" name="retiva" id="retiva" placeholder="000000" readonly>
                                 </div>
                             </div>
-                            <div class="col-xs-12 col-sm-2 col-md-2">
+                            <div class="col-xs-12 col-sm-6 col-md-2">
                                 <div class="mb-3">
                                     <label for="exampleFormControlInput1" class="form-label">Rete ICA</label>
                                     <input type="text" class="form-control" name="retica" id="retica" placeholder="000000" readonly>
                                 </div>
                             </div>
-                            <div class="col-xs-12 col-sm-2 col-md-2">
+                            <div class="col-xs-12 col-sm-6 col-md-2">
                                 <div class="mb-3">
                                     <label for="exampleFormControlInput1" class="form-label">Banco</label>
                                     <input type="text" class="form-control" name="banco" id="banco" placeholder="000000" readonly>
                                 </div>
                             </div>
-                            <div class="col-xs-12 col-sm-2 col-md-2">
+                            <div class="col-xs-12 col-sm-6 col-md-2">
                                 <div class="mb-3">
                                     <label for="exampleFormControlInput1" class="form-label">Diferencia</label>
                                     <input type="text" class="form-control" name="diferencia" id="diferencia" placeholder="000000" readonly>
@@ -154,7 +154,7 @@
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-xs-12 col-sm-12 col-md-12">
+                            <div class="col-xs-12 col-sm-6 col-md-2">
                                 <div >
                                     <button type="submit" class="btn btn-success" >Agregar</button>
                                 </div>
